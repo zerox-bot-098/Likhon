3429669fe512eefab1848863c3d89f561d6ad2db
zerox-bot-098 <samolhossen098@gmail.com>
2025-08-31 23:21:37 +0600

    Update baby.js

--- Script/commands/baby.js
+++ Script/commands/baby.js
@@ -1,5 +1,10 @@
 const axios = require("axios");
-const simsim = "https://cyber-simsimi.onrender.com";
+{
+  "author": "ULLASH × IMRAN",
+  "code": 200,
+  "message": "𝐂𝐘𝐁𝐄𝐑'𝐒 𝐉𝐀𝐍 𝐒𝐈𝐌 𝐀𝐏𝐈 𝐈𝐒 𝐑𝐔𝐍𝐍𝐈𝐍𝐆 𝐎𝐍𝐋𝐈𝐍𝐄",
+  "use": "/simsimi?text=Hi | /teach?ask=Hi&ans=Hello | /list | /edit | /delete | /baby?cmd=top-t"
+}
 
 module.exports.config = {
  name: "baby",
